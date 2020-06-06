23ad78e090ec12ba874b51060f3281208dbb85ab
fddandy <d.fedorinenko39@gmail.com>
2020-06-06 16:44:11 +0200

    Добавьте файлы проекта.

---
+++ Saper/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Saper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.graToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.prostaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sredniaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.trudnaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panelButtons = new System.Windows.Forms.Panel();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.graToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // graToolStripMenuItem
+            // 
+            this.graToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.prostaToolStripMenuItem,
+            this.sredniaToolStripMenuItem,
+            this.trudnaToolStripMenuItem});
+            this.graToolStripMenuItem.Name = "graToolStripMenuItem";
+            this.graToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.graToolStripMenuItem.Text = "Gra";
+            // 
+            // prostaToolStripMenuItem
+            // 
+            this.prostaToolStripMenuItem.Name = "prostaToolStripMenuItem";
+            this.prostaToolStripMenuItem.Size = new System.Drawing.Size(113, 22);
+            this.prostaToolStripMenuItem.Text = "Prosta";
+            this.prostaToolStripMenuItem.Click += new System.EventHandler(this.prostaToolStripMenuItem_Click);
+            // 
+            // sredniaToolStripMenuItem
+            // 
+            this.sredniaToolStripMenuItem.Name = "sredniaToolStripMenuItem";
+            this.sredniaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.sredniaToolStripMenuItem.Text = "Srednia";
+            this.sredniaToolStripMenuItem.Click += new System.EventHandler(this.sredniaToolStripMenuItem_Click);
+            // 
+            // trudnaToolStripMenuItem
+            // 
+            this.trudnaToolStripMenuItem.Name = "trudnaToolStripMenuItem";
+            this.trudnaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.trudnaToolStripMenuItem.Text = "Trudna";
+            this.trudnaToolStripMenuItem.Click += new System.EventHandler(this.trudnaToolStripMenuItem_Click);
+            // 
+            // panelButtons
+            // 
+            this.panelButtons.AutoSize = true;
+            this.panelButtons.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panelButtons.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelButtons.Location = new System.Drawing.Point(0, 24);
+            this.panelButtons.Name = "panelButtons";
+            this.panelButtons.Size = new System.Drawing.Size(800, 426);
+            this.panelButtons.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panelButtons);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Saper v0.1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem graToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem prostaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sredniaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem trudnaToolStripMenuItem;
+        private System.Windows.Forms.Panel panelButtons;
+    }
+}
+
---
+++ Saper/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Saper
+{
+    public partial class Form1 : Form
+    {
+        private SaperLogic myGame;
+        private const int fieldSize = 30;
+        public Form1()
+        {
+            InitializeComponent();
+            prostaToolStripMenuItem_Click(null, null);
+        }
+
+        private void prostaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //obsluga dynamicznego buttona 
+            /*
+            Button b = new Button();
+            b.Location = new Point(50, 50);
+            b.Size = new Size(100, 100);
+            b.Text = "Dynamiczny Button";
+            b.Click += B_Click;
+            this.Controls.Add(b);
+            */
+            myGame = new SaperLogic(8, 8, 10);
+            generateView();
+        }
+
+        
+
+        private void sredniaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            myGame = new SaperLogic(12, 10, 25);
+            generateView();
+        }
+
+        private void trudnaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            myGame = new SaperLogic(20, 15, 50);
+            generateView();
+        }
+
+        private void generateView()
+        {
+            panelButtons.Controls.Clear();
+            for(int x = 0; x<myGame.BoardWidth; x++)
+                { 
+
+                for(int y = 0; y<myGame.BoardHeight; y++)
+                {
+                    Button b = new Button();
+                    b.Size = new Size(fieldSize, fieldSize);
+                    b.Location = new Point(fieldSize * x, fieldSize * y);
+                    b.Click += button_Click;
+                    panelButtons.Controls.Add(b);
+
+                    b.Tag = new Point(x, y);
+
+                }
+            }
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            if(myGame.State == SaperLogic.GameState.InProgress)
+            {
+                if(sender is Button)
+                {
+                    Button b = sender as Button;
+                    if(b.Tag is Point)
+                    {
+                        //point jest struktura, wiec nie mozna uzyc konstrukcji @sender as Point@
+                        Point p = (Point)b.Tag;
+                        myGame.Uncover(p);
+                        refreshView();
+                        if(myGame.State == SaperLogic.GameState.Win)
+                        {
+                            MessageBox.Show("Wygrana");
+                        }
+                        else if (myGame.State == SaperLogic.GameState.Loss)
+                        {
+                            MessageBox.Show("Przegrana");
+                        }
+                    }
+                }
+            }
+        }
+
+        private void refreshView()
+        {
+            foreach(Button b in panelButtons.Controls)
+            {
+                SaperLogic.Field f = myGame.GetField((Point)b.Tag);
+                if(f.Covered == false)
+                {
+                    if(f.FieldType == SaperLogic.FieldTypeEnum.Bomb)
+                    {
+                        b.BackColor = Color.Red;
+                        b.Text = "@";
+                    }
+                    else
+                    {
+                        b.BackColor = Color.White;
+                        if(f.FieldType == SaperLogic.FieldTypeEnum.BombCount)
+                        {
+                            b.Text = f.BombCount.ToString();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
